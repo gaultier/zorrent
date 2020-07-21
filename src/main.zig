2f6b542816c215e7821c0caa50403f2685e35f3b
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-07-21 10:52:38 +0200

    Move utility functions to lib

--- src/main.zig
+++ src/main.zig
@@ -2,102 +2,6 @@ const std = @import("std");
 const c = @cImport(@cInclude("curl/curl.h"));
 const bencode = @import("zig-bencode");
 
-fn outputUnicodeEscape(
-    codepoint: u21,
-    out_stream: var,
-) !void {
-    if (codepoint <= 0xFFFF) {
-        // If the character is in the Basic Multilingual Plane (U+0000 through U+FFFF),
-        // then it may be represented as a six-character sequence: a reverse solidus, followed
-        // by the lowercase letter u, followed by four hexadecimal digits that encode the character's code point.
-        try out_stream.writeAll("\\u");
-        try std.fmt.formatIntValue(codepoint, "x", std.fmt.FormatOptions{ .width = 4, .fill = '0' }, out_stream);
-    } else {
-        std.debug.assert(codepoint <= 0x10FFFF);
-        // To escape an extended character that is not in the Basic Multilingual Plane,
-        // the character is represented as a 12-character sequence, encoding the UTF-16 surrogate pair.
-        const high = @intCast(u16, (codepoint - 0x10000) >> 10) + 0xD800;
-        const low = @intCast(u16, codepoint & 0x3FF) + 0xDC00;
-        try out_stream.writeAll("\\u");
-        try std.fmt.formatIntValue(high, "x", std.fmt.FormatOptions{ .width = 4, .fill = '0' }, out_stream);
-        try out_stream.writeAll("\\u");
-        try std.fmt.formatIntValue(low, "x", std.fmt.FormatOptions{ .width = 4, .fill = '0' }, out_stream);
-    }
-}
-
-fn dump(value: *bencode.Value, indent: usize) anyerror!void {
-    var out_stream = std.io.getStdOut().writer();
-
-    switch (value.*) {
-        .Integer => |n| {
-            try out_stream.print("{}", .{n});
-        },
-        .String => |s| {
-            var i: usize = 0;
-
-            try out_stream.print("\"", .{});
-            if (std.unicode.utf8ValidateSlice(s)) {
-                while (i < s.len) : (i += 1) {
-                    switch (s[i]) {
-                        // normal ascii character
-                        0x20...0x21, 0x23...0x2E, 0x30...0x5B, 0x5D...0x7F => |ch| try out_stream.writeByte(ch),
-                        // only 2 characters that *must* be escaped
-                        '\\' => try out_stream.writeAll("\\\\"),
-                        '\"' => try out_stream.writeAll("\\\""),
-                        // solidus is optional to escape
-                        '/' => {
-                            try out_stream.writeByte('/');
-                        },
-                        // control characters with short escapes
-                        // TODO: option to switch between unicode and 'short' forms?
-                        0x8 => try out_stream.writeAll("\\b"),
-                        0xC => try out_stream.writeAll("\\f"),
-                        '\n' => try out_stream.writeAll("\\n"),
-                        '\r' => try out_stream.writeAll("\\r"),
-                        '\t' => try out_stream.writeAll("\\t"),
-                        else => {
-                            const ulen = std.unicode.utf8ByteSequenceLength(s[i]) catch unreachable;
-                            // control characters (only things left with 1 byte length) should always be printed as unicode escapes
-                            if (ulen == 1) {
-                                const codepoint = std.unicode.utf8Decode(s[i .. i + ulen]) catch unreachable;
-                                try outputUnicodeEscape(codepoint, out_stream);
-                            } else {
-                                try out_stream.writeAll(s[i .. i + ulen]);
-                            }
-                            i += ulen - 1;
-                        },
-                    }
-                }
-            } else {
-                for (s) |ch| {
-                    try out_stream.print("\\x{X}", .{ch});
-                }
-            }
-            try out_stream.print("\"", .{});
-        },
-        .Array => |arr| {
-            for (arr.items) |*v| {
-                try out_stream.print("\n", .{});
-                try out_stream.writeByteNTimes(' ', indent);
-                try out_stream.print("- ", .{});
-                try dump(v, indent + 2);
-            }
-        },
-        .Object => |*obj| {
-            var node = obj.first();
-            while (node) |it| {
-                const entry = bencode.mapGetEntry(it);
-                try out_stream.print("\n", .{});
-                try out_stream.writeByteNTimes(' ', indent);
-                try out_stream.print("\"{}\": ", .{entry.key});
-                try dump(&entry.value, indent + 2);
-
-                node = it.next();
-            }
-        },
-    }
-}
-
 fn writeCallback(p_contents: *c_void, size: usize, nmemb: usize, p_user_data: *std.ArrayList(u8)) usize {
     const contents = @ptrCast([*c]const u8, p_contents);
     p_user_data.*.appendSlice(contents[0..nmemb]) catch {
@@ -207,7 +111,7 @@ pub fn main() anyerror!void {
     std.debug.warn("Res body: {}", .{res_body.items});
 
     var value_decoded = try bencode.ValueTree.parse(res_body.items[0..], allocator);
-    try dump(&value_decoded.root, 0);
+    try bencode.dump(&value_decoded.root, 0);
 
     var dict = value_decoded.root.Object;
     // TODO: support non compact format i.e. a list of strings
