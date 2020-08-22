73b947ada89c5aaa41bedb040b731fcddd0ac79c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-08-22 13:36:19 +0200

    fix ceil

--- src/utils.zig
+++ src/utils.zig
@@ -1,3 +1,3 @@
 pub fn ceil(comptime T: type, numerator: T, denumerator: T) T {
-    return @floatToInt(T, std.math.ceil(f64, @intToFloat(f64, numerator) / @intToFloat(f64, denumerator)));
+    return 1 + (numerator - 1) / denumerator;
 }
