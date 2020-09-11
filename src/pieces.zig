d2f09b5e9097048b4dfbe2f28a885ba74d4887d5
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-09-11 21:32:34 +0200

    rm special handling of indices

--- src/pieces.zig
+++ src/pieces.zig
@@ -136,7 +136,7 @@ pub const Pieces = struct {
             };
             files.addOneAssumeCapacity().* = file;
 
-            const len = file_sizes[if (file_paths.len > 1) i - 1 else i];
+            const len = file_sizes[i];
             try std.os.ftruncate(file.handle, len);
 
             if (file_exists) {
@@ -246,7 +246,7 @@ pub const Pieces = struct {
                 {
                     var total_len_so_far: usize = 0;
                     for (self.files) |f, i| {
-                        const len = self.file_sizes[if (self.file_paths.len > 1) i + 1 else i];
+                        const len = self.file_sizes[i];
                         if (total_len_so_far <= file_offset and file_offset < total_len_so_far + len) {
                             file = f;
                             break;
